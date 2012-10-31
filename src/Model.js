1f59ffb2e2c840d5a246d6c3000dc8da6352f7de
Amit Bansil <amit@amit-bansil.com>
2012-10-30 22:36:57 -0400

    use strict

--- src/Model.js
+++ src/Model.js
@@ -1,19 +1,23 @@
 /*global _:true, balze:true */
+(function () {
+    "use strict";
 
-blaze.Model = function(setup) {
-	this.getGridSize = _.constant(setup.gridSize);
-};
+	blaze.Model = function(setup) {
+		this.getGridSize = _.constant(setup.gridSize);
+	};
 
-blaze.Model.prototype.restart = function() {
-};
+	blaze.Model.prototype.restart = function() {
+	};
 
-blaze.Model.prototype.isBurning = function() {
-};
+	blaze.Model.prototype.isBurning = function() {
+	};
 
-blaze.Model.prototype.step = function() {
-};
+	blaze.Model.prototype.step = function() {
+	};
 
-blaze.Square = function(x, y) {
-	this.getX = _.constant(x);
-	this.getY = _.constant(y);
-};
+	blaze.Square = function(x, y) {
+		this.getX = _.constant(x);
+		this.getY = _.constant(y);
+	};
+
+}());
--- src/View.js
+++ src/View.js
@@ -1,35 +1,39 @@
 /*global _:true, blaze:true, $:true */
+(function () {
+    "use strict";
 
-blaze.View = function(model) {
-	this.model = model;
-	this.canvas = $("#canvas");
+	blaze.View = function(model) {
+		this.model = model;
+		this.canvas = $("#canvas");
 
-	//resize canvas to correct height
-	var canvas_wrap = $("#canvas_wrap");
-	var height = canvas_wrap.innerHeight() - 20;
-	this.canvas[0].width = height;
-	this.canvas[0].height = height;
-	canvas_wrap.height(height);
+		//resize canvas to correct height
+		var canvas_wrap = $("#canvas_wrap");
+		var height = canvas_wrap.innerHeight() - 20;
+		this.canvas[0].width = height;
+		this.canvas[0].height = height;
+		canvas_wrap.height(height);
 
-	this.ctx = this.canvas[0].getContext("2d");
+		this.ctx = this.canvas[0].getContext("2d");
 
-	var width = this.canvas.width();
+		var width = this.canvas.width();
 
-	this.ctx.scale(width, height);
+		this.ctx.scale(width, height);
 
-	this.pixel = 1 / width;
+		this.pixel = 1 / width;
 
-	this.canvas.click(_.bind(this._mouseClick, this));
-	this.canvas.mousemove(_.bind(this._mouseMove, this));
-	this.canvas.mouseleave(_.bind(this._mouseLeave, this));
-};
+		this.canvas.click(_.bind(this._mouseClick, this));
+		this.canvas.mousemove(_.bind(this._mouseMove, this));
+		this.canvas.mouseleave(_.bind(this._mouseLeave, this));
+	};
 
-blaze.View.prototype._mouseClick = function(event) {
-};
-blaze.View.prototype._mouseMove = function(event) {
-};
-blaze.View.prototype._mouseLeave = function(event) {
-};
+	blaze.View.prototype._mouseClick = function(event) {
+	};
+	blaze.View.prototype._mouseMove = function(event) {
+	};
+	blaze.View.prototype._mouseLeave = function(event) {
+	};
 
-blaze.View.prototype.update = function() {
-};
+	blaze.View.prototype.update = function() {
+	};
+
+}());
